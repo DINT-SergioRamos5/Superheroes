f7ffc003fb64598e6590c432135a7c183cc6ed25
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-10-29 12:55:24 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/Configuracion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes
+{
+    class Configuracion : INotifyPropertyChanged
+    {
+        public string AjusteImagen { get; set; }
+        public string Fondo { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Configuracion()
+        {
+
+        }
+
+        
+    }
+}
---
+++ Superheroes/Superheroe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes
+{
+    public class Superheroe : INotifyPropertyChanged
+    {
+        public string Nombre { get; set; }
+        public string Enemigo { get; set; }
+        public string Foto { get; set; }
+        public bool Vengador { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //La interrogacion se introduce para saber si el resultado de la propiedad es nulo.
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public Superheroe()
+        {
+        }        
+
+        public static Superheroe GetSample()
+        {
+            Superheroe heroe = new Superheroe();
+            heroe.Nombre = "Daredevil";
+            heroe.Enemigo = "Kingping";
+            heroe.Foto = @"https://dam.smashmexico.com.mx/wp-content/uploads/2018/08/27150601/daredevilbio_portada.jpg";
+            heroe.Vengador = false;
+
+            return heroe;
+        }
+    }
+}
